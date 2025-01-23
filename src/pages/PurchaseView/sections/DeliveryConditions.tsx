3489c12a74fe09b0c6ab7ea5119d7459caa60a9b
Alina Orekhova <orehus.lina@mail.ru>
2025-01-24 02:52:21 +0400

    Delivery Conditions Section

--- src/pages/PurchaseView/sections/DeliveryConditions.tsx
+++ src/pages/PurchaseView/sections/DeliveryConditions.tsx
@@ -1,10 +1,52 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import { Box, TextField, Grid } from '@mui/material';
 
-const DeliveryConditions: React.FC = () => (
-  <Box sx={{ padding: 2, backgroundColor: '#fff' }}>
-    <p>Контент для раздела "Условия поставки".</p>
-  </Box>
-);
+const DeliveryConditions: React.FC = () => {
+  return (
+    <Box sx={{ padding: 3 }}>
+      <Grid container spacing={2}>
+        {/* Отсрочка */}
+        <Grid item xs={12} sm={6}>
+          <TextField
+            label="Отсрочка"
+            defaultValue="30 дней"
+            fullWidth
+            variant="outlined"
+          />
+        </Grid>
+
+        {/* Способ доставки */}
+        <Grid item xs={12} sm={6}>
+          <TextField
+            label="Способ доставки"
+            defaultValue="Самовывоз"
+            fullWidth
+            variant="outlined"
+          />
+        </Grid>
+
+        {/* Предоплата */}
+        <Grid item xs={12} sm={6}>
+          <TextField
+            label="Предоплата"
+            defaultValue="30%"
+            fullWidth
+            variant="outlined"
+          />
+        </Grid>
+
+        {/* Обрешетка */}
+        <Grid item xs={12} sm={6}>
+          <TextField
+            label="Обрешетка"
+            defaultValue="Не требуется"
+            fullWidth
+            variant="outlined"
+          />
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
 
 export default DeliveryConditions;
